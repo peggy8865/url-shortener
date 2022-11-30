31ad879e4a29abb315674b5bc20fe21ca2ca7cdf
peggy8865 <peggy8865@gmail.com>
2022-11-30 20:52:34 +0800

    feat: project init

---
+++ models/urls.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlSchema = new Schema({
+  url: {
+    type: String,
+    required: true
+  },
+  shortUrl: {
+    type: String,
+  }
+})
+
+module.exports = mongoose.model('URL', urlSchema)
